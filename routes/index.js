20eac6c4c90f211b4efafc25a15af48e5ab4af36
Griffin Hammer <grifhammer@gmail.com>
2015-12-14 16:28:19 -0500

    Added login using passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/register', function (req, res, next){
+    res.render('index', {title: 'Register!'});
+});
+
+router.post('/register', function (req, res, next){
+    Account.register(new Account({
+        username: req.body.username,
+        password: req.body.password
+    }), function (error, account){
+        if (error){
+            console.log(error);
+            res.render('index');
+        }else{
+            passport.authenticate('local') (req, res, function(){
+                res.redirect('/');
+            })
+        }
+    });
+})
 
 module.exports = router;
