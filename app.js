6b61e4b359287b4a03247550c44adfef582ac613
Griffin Hammer <grifhammer@gmail.com>
2015-12-14 15:01:11 -0500

    Initialized passport.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
 
 var routes = require('./routes/index');
@@ -24,15 +25,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')){
-  secret: 'dc-4lyfe'
-  resave: false;
-  saveUninitialized: false;
-};
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, 
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+mongoose.connect('mongodb://localhost:27017/coffee')
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
