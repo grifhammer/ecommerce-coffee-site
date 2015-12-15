f0427c37266325e1ad62e02c8941c6178ad4a971
Griffin Hammer <grifhammer@gmail.com>
2015-12-15 14:54:25 -0500

    Added the rest of the registration page and fixed routing.

--- routes/index.js
+++ routes/index.js
@@ -5,31 +5,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log(req.user)
   res.render('index', { title: 'Express' });
 });
 
 router.get('/register', function (req, res, next){
-    res.render('index', {title: 'Register!'});
+    res.render('register');
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            console.log(err);
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
 });
 
 router.get('/login', function (req, res, next){
     res.render('login');
 });
 
-router.post('/register', function (req, res, next){
-    Account.register(new Account({
-        username: req.body.username,
-        password: req.body.password
-    }), function (error, account){
-        if (error){
-            console.log(error);
-            res.render('index');
-        }else{
-            passport.authenticate('local') (req, res, function(){
-                res.redirect('/');
-            })
-        }
-    });
-})
+router.post('/login', function (req, res, next){
+    res.redirect('/')
+});
+
 
 module.exports = router;
